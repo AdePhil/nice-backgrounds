9e30a034ac0bc1354694fd09eadfeaf048a28616
Adebisi Ahmed <adebisi.ahmed10@gmail.com>
2022-01-25 11:57:59 +0100

    refactor: move data files

---
+++ data/index.js
@@ -0,0 +1,22 @@
+export const colors = [
+  {
+    id: 1,
+    name: "Bg1",
+    background: "rgb(17, 24, 39)",
+  },
+  {
+    id: 2,
+    name: "Bg2",
+    background: "linear-gradient(180deg, #181823, #181823)",
+  },
+  {
+    id: 3,
+    name: "Bg3",
+    background: "#000",
+  },
+  {
+    id: 4,
+    name: "Bg3",
+    background: "#1f2028",
+  },
+];
--- pages/index.js
+++ pages/index.js
@@ -4,29 +4,7 @@ import { useState } from "react";
 import { Flex, Heading, Text } from "rebass";
 import { Box } from "rebass";
 import { AnimateSharedLayout, motion } from "framer-motion";
-
-const colors = [
-  {
-    id: 1,
-    name: "Bg1",
-    background: "rgb(17, 24, 39)",
-  },
-  {
-    id: 2,
-    name: "Bg2",
-    background: "linear-gradient(180deg, #181823, #181823)",
-  },
-  {
-    id: 3,
-    name: "Bg3",
-    background: "#000",
-  },
-  {
-    id: 4,
-    name: "Bg3",
-    background: "#1f2028",
-  },
-];
+import { colors } from "../data";
 
 const MotionFlex = motion(Flex);
 const MotionBox = motion(Box);
