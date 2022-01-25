4abd9851c2a97e63e15fdf385525ad038f96e7c2
Adebisi Ahmed <adebisi.ahmed10@gmail.com>
2022-01-25 10:44:37 +0100

    feat: initial commit

---
+++ components/fonts/index.js
@@ -0,0 +1,6 @@
+const Fonts = () => (
+  <style jsx global>{`
+    @import url("https://fonts.googleapis.com/css2?family=Sen:wght@400;500;600;700&display=swap");
+  `}</style>
+);
+export default Fonts;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import { ThemeProvider } from "emotion-theming";
+import Fonts from "../components/fonts";
+import GlobalStyles from "../lib/globalstyles";
+import theme from "../lib/theme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Fonts />
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
